679016b717e796d10e1ad5d9bc0f9005a529e3f7
Ruslan Simakov <ruslan.simakov@spirent.com>
2022-01-04 12:34:52 +0200

    added akka streams base

---
+++ akkaStream/src/main/java/ruslan/simakov/AkkaStreamApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkaStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkkaStreamApplication.class, args);
+    }
+
+}
