0963e8854eed71397f67662867e073a2bc6138cd
Ruslan Simakov <ruslan.simakov@spirent.com>
2022-01-17 01:15:45 +0200

    Add Eureka-server

--- akkaStream/src/main/java/ruslan/simakov/AkkaStreamApplication.java
+++ akkaStream/src/main/java/ruslan/simakov/AkkaStreamApplication.java
@@ -1,13 +1,21 @@
 package ruslan.simakov;
 
+import akka.NotUsed;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Source;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.math.BigInteger;
 
 @SpringBootApplication
 public class AkkaStreamApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AkkaStreamApplication.class, args);
+//        SpringApplication.run(AkkaStreamApplication.class, args);
+
+        Source<Integer, NotUsed> source = Source.range(1,200);
+        Flow<Integer, BigInteger,NotUsed> flow;
     }
 
 }
